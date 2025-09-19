ff75e24d345e7a3606fd505773df0c724749e72f
Evelyn <git@evelyn.moe>
2025-09-19 05:37:14 +0100

    Force use of STOMP 1.2, rel #2

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
 
     # https://stomp.github.io/stomp-specification-1.2.html#Heart-beating
     # We're committing to sending and accepting heartbeats every 5000ms
-    connection = stomp.Connection([('publicdatafeeds.networkrail.co.uk', 61618)], keepalive=True, heartbeats=(5000, 5000))
+    connection = stomp.Connection12([('publicdatafeeds.networkrail.co.uk', 61618)], keepalive=True, heartbeats=(5000, 5000))
     connection.set_listener('', Listener(connection))
 
     # Connect to feed
