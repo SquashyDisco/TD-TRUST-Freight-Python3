d744f48d9ba39a82485ae5d058a8ba5eb4f1b9c7
SquashyDisco <114489076+SquashyDisco@users.noreply.github.com>
2026-08-27 23:16:27 +0100

    Update trust.py

--- util/trust.py
+++ util/trust.py
@@ -1,14 +1,14 @@
 TOCS = {}
 
 MESSAGES = {
-    "0001": "activation",
-    "0002": "cancellation",
-    "0003": "movement",
-    "0004": "_unidentified",
-    "0005": "reinstatement",
-    "0006": "origin change",
-    "0007": "identity change",
-    "0008": "_location change"
+    "0001": "ACTIVATED",
+    "0002": "CANCELLED",
+    "0003": "PASS",
+    "0004": "_unidentified ",
+    "0005": "REINSTATE",
+    "0006": "NEWORIGIN",
+    "0007": "IDENTITY",
+    "0008": "LOCATION"
     }
 
 
@@ -18,10 +18,17 @@ def print_trust_frame(parsed):
 
         toc = a["body"].get("toc_id", '')
         platform = a["body"].get("platform", '')
-        loc_stanox = "@" + body.get("loc_stanox", "")
+        loc_stanox = a["body"].get("loc_stanox", "")
+        actual_timestamp = a["body"].get("actual_timestamp", '')
+        variation_status = a["body"].get("variation_status", '')
+        lateness = a["body"].get("timetable_variation",'')
+        direction = a["body"].get("direction_ind"," ")
+        line = a["body"].get("line_ind"," ")
+        
 
-        summary = "{} ({} {}) {:<13s} {:2s} {:<6s} {:3s}".format(
-            body["train_id"], body["train_id"][2:6], body["train_id"][6],
-            MESSAGES[a["header"]["msg_type"]], toc, loc_stanox, platform)
+        summary = "{} {} {:9s} {:3s} {:4s} {} {:<6s} {} {:3s} {}".format(
+            toc,
+            body["train_id"][2:6],
+            MESSAGES[a["header"]["msg_type"]], platform, direction, line, loc_stanox, actual_timestamp, lateness, variation_status)
 
         print(summary)
